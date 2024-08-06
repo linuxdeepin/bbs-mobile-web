c79ba4fbcccaa82c01de2ae2b24ea9f03d8b09bb
student-ice <tonimayloneya@gmail.com>
2024-08-06 10:16:20 +0800

    feat: index page support pull down refresh
    支持首页下拉刷新

--- src/pages/index/index.config.ts
+++ src/pages/index/index.config.ts
@@ -1,5 +1,7 @@
 export default definePageConfig({
   enableShareTimeline: true,
   enableShareAppMessage: true,
+  enablePullDownRefresh: true,
   navigationStyle: "custom",
+  backgroundTextStyle: "dark",
 });
