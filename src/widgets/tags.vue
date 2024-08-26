5bc0bb2ca5a3c3207679b5faf26313aff41f3002
ice909 <tonimayloneya@gmail.com>
2024-08-26 11:35:33 +0800

    perf: reduce the number of network requests to get user tags
    减少获取用户标签时的网络请求次数
    避免同一用户标签多次重复获取

--- src/widgets/tags.vue
+++ src/widgets/tags.vue
@@ -1,31 +1,21 @@
 <template>
   <view class="level-container">
-    <nut-tag class="tag" v-if="userGroupName !== ''" color="#1979ff" plain>{{ userGroupName }}</nut-tag>
+    <nut-tag class="tag" v-if="groupName !== ''" color="#1979ff" plain>{{ groupName }}</nut-tag>
     <!-- tags -->
-    <template v-for="tag in tags">
-      <nut-tag class="tag" :color="tag.color" plain>{{ tag.text }}</nut-tag>
+    <template v-if="userTags">
+      <nut-tag v-for="tag in userTags" class="tag" :color="tag.color" plain>{{ tag.text }}</nut-tag>
     </template>
   </view>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
-import { GetUserTag, UserTags, GetUserInfo } from '@/api';
+import { UserTags } from '@/api';
 
-const props = defineProps<{
-  userId: number,
+defineProps<{
+  groupName: string,
+  userTags: UserTags[],
 }>()
 
-const tags = ref<UserTags[]>([])
-const userGroupName = ref("")
-
-onMounted(async () => {
-  const res = await GetUserTag(props.userId)
-  // 只保留lang 为 zh_CN的tag
-  tags.value = res.data.filter(tag => tag.lang === 'zh_CN')
-  const userInfo = await GetUserInfo(props.userId)
-  userGroupName.value = userInfo.data.group_name
-})
 </script>
 
 <style lang="scss">
