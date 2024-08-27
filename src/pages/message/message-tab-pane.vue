12a8caf4b51b1cf1c5a29402ff5e86b0a5851dfe
ice909 <tonimayloneya@gmail.com>
2024-08-27 11:48:33 +0800

    feat: optimize the message module code
    优化消息模块代码

--- src/pages/message/message-tab-pane.vue
+++ src/pages/message/message-tab-pane.vue
@@ -9,65 +9,7 @@
     <view v-if="!props.isLoading" class="message-list">
       <template v-if="totalCount">
         <template v-for="item in props.data">
-          <nut-cell-group @click="clickMessage(item)">
-            <!-- 用户头像,昵称，时间 -->
-            <nut-cell desc-text-align="left">
-              <template #icon>
-                <nut-badge :dot="!item.is_new">
-                  <nut-avatar size="40" shape="round">
-                    <img v-if="item.category !== 3" :src="item.send_user_avatar" />
-                    <Notice v-else />
-                  </nut-avatar>
-                </nut-badge>
-              </template>
-              <template v-if="item.category !== 5" #desc>
-                <view class="info-desc">
-                  <view class="info">
-                    <span class="nickname"> {{ item.category === 3 ? item.type : item.send_user_nickname }}</span>
-                    <span class="time">{{ formatTime(item.created_at) }}</span>
-                  </view>
-                  <view class="del-btn">
-                    <Del @click.stop="delBtnClicked(item.id)" />
-                  </view>
-                </view>
-              </template>
-              <template v-else #desc>
-                <view class="letter-content">
-                  <view class="top">
-                    <span class="nickname"> {{ item.send_user_nickname }} </span>
-                    <span class="time">{{ formatTime(item.created_at) }}</span>
-                  </view>
-                  <view class="bottom">
-                    <view class="content">{{ item.send_message_fmt }}</view>
-                    <Del @click.stop="delBtnClicked(item.id)" />
-                  </view>
-                </view>
-              </template>
-            </nut-cell>
-            <!-- 回复和帖子标题 -->
-            <nut-cell v-if="item.category !== 5">
-              <template #default>
-                <view v-if="item.category !== 3" class="message-content">
-                  <view class="reply-info">
-                    <span class="title">{{ item.category === 6 ? "@我的" : "回复" }}:&nbsp;</span>
-                    <span class="content">{{ item.message_fmt }}</span>
-                  </view>
-                  <view class="post-info" v-if="item.category === 2">
-                    <span class="nickname">{{ item.receive_user_nickname }}:&nbsp;</span>
-                    <span class="content">{{ item.send_message_fmt }}</span>
-                  </view>
-                  <view class="thread-title">
-                    {{ item.subject }}
-                  </view>
-                </view>
-                <view v-else class="message-content">
-                  <view class="content">
-                    {{ item.note }}
-                  </view>
-                </view>
-              </template>
-            </nut-cell>
-          </nut-cell-group>
+          <MessageItem :message="item" />
         </template>
       </template>
       <view v-else>
@@ -83,56 +25,26 @@
 </template>
 
 <script lang="ts" setup>
-import { Datum, MessageRead, MessageReadAll, PostOffset } from '@/api';
-import Taro from '@tarojs/taro'
-import { Del, Notice } from "@nutui/icons-vue-taro";
-import { formatTime } from "@/utils/format"
+import { MessageDatum, MessageReadAll } from '@/api';
+import MessageItem from "./message-item.vue"
 
 const emit = defineEmits<{
   pageTurning: [number],
   refresh: [],
-  showDelDialog: [number]
 }>()
 
 const props = defineProps<{
-  data: Datum[],
+  data: MessageDatum[],
   isLoading: Boolean,
   totalCount: number,
   newMsgCount: number,
-  category: number,
 }>()
 
-const clickMessage = async (datum: Datum) => {
-  // 已读当前消息
-  MessageRead({ id: datum.id })
-  if (datum.category === 3) {
-    Taro.navigateTo({
-      url: `/pages/notify/notify?title=${encodeURIComponent(datum.type)}&content=${encodeURIComponent(datum.note)}&time=${encodeURIComponent(formatTime(datum.created_at))}`,
-    })
-    return
-  }
-  if (datum.category === 5) {
-    Taro.navigateTo({
-      url: `/pages/dialog/dialog?uid=${datum.send_user_id}`,
-    })
-    return
-  }
-  const res = await PostOffset({ id: datum.send_post_id, thread_id: datum.thread_id, page_size: 10 })
-  Taro.navigateTo({
-    url: `/pages/thread/thread?id=${datum.thread_id}&postId=${datum.send_post_id}&offset=${res.data.offset}`,
-  })
-}
-
 const readAllMsg = async () => {
-  const res = await MessageReadAll({ category: props.category })
+  const res = await MessageReadAll({ category: props.data[0].category })
   if (res.data.code === 0) {
     emit('refresh')
   }
-}
-
-const delBtnClicked = (id: number) => {
-  console.log('delBtnClicked', id)
-  emit('showDelDialog', id)
 }
 </script>
 
@@ -150,111 +62,6 @@ const delBtnClicked = (id: number) => {
       color: var(--text-desc-color);
       font-size: 16Px;
     }
-  }
-
-  .message-list {
-    .info-desc {
-      margin-left: 10px;
-      width: 100%;
-      display: flex;
-      justify-content: space-between;
-
-      .info {
-        display: flex;
-        flex-direction: column;
-      }
-
-      .nickname {
-        color: var(--text-desc-color);
-      }
-
-      .time {
-        color: var(--text-desc-color);
-      }
-    }
-
-    .letter-content {
-      display: flex;
-      flex-direction: column;
-
-      .top {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-
-        .nickname {
-          color: var(--text-desc-color);
-        }
-
-        .time {
-          color: var(--text-desc-color);
-        }
-      }
-
-      .bottom {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-top: 10rpx;
-
-        .content {
-          flex: 1;
-          color: var(--text-desc-color);
-          overflow: hidden;
-          text-overflow: ellipsis;
-          display: -webkit-box;
-          -webkit-line-clamp: 1;
-          -webkit-box-orient: vertical;
-        }
-      }
-    }
-
-    .message-content {
-      display: block;
-      width: 100%;
-
-      .content {
-        padding: 5px 0;
-        color: var(--text-desc-color);
-        /** 最多显示两行，超出显示省略号 **/
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-      }
-
-      .reply-info {
-        display: flex;
-
-        .title {
-          color: var(--text-desc-color);
-          white-space: nowrap;
-          padding: 5px 0;
-        }
-      }
-
-      .post-info {
-        margin: 10px 0;
-        display: flex;
-
-        .nickname {
-          padding: 5px 0;
-          color: var(--text-desc-color);
-          white-space: nowrap;
-        }
-      }
-
-      .thread-title {
-        display: block;
-        background-color: var(--reply-bg-color);
-        color: var(--text-desc-color);
-        border-radius: 4px;
-        padding: 10px 10px;
-        margin-top: 10px;
-      }
-    }
-
   }
 
   .skeleton-container {
